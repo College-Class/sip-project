b157daebc12a5820b546be09a169b0166040dbd8
rabinacharya11 <charms2074@gmail.com>
2024-03-03 10:03:22 +0545

    fixes

--- components/heroSection.jsx
+++ components/heroSection.jsx
@@ -5,7 +5,7 @@ function HeroSection() {
   <div className="w-[1391px] h-[444px] flex justify-center items-center gap-[138px]">
     <div className="flex-col justify-start items-start gap-6 inline-flex">
         <div className="text-black text-4xl font-bold ">Beyond Bias, Towards Unity</div>
-        <div className="w-[557px] text-black text-lg font-normal ">Embark on a transformative journey with us—beyond biases, towards unity. We're dedicated to breaking down barriers, fostering a world where equality triumphs over discrimination. Join our movement for a future where unity prevails</div>
+        <div className="w-[557px] text-black text-lg font-normal ">Embark on a transformative journey with us—beyond biases, towards unity. We are dedicated to breaking down barriers, fostering a world where equality triumphs over discrimination. Join our movement for a future where unity prevails</div>
         <div className="w-[304px] h-[72px] relative">
             <div className="w-[304px] h-[72px] left-0 top-0 absolute bg-slate-900 rounded-xl" />
             <div className="left-[50px] top-[21px] absolute text-white text-24px font-normal ">Register a Complain </div>
